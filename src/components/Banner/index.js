bb432eada132a16fbcb6cff798ddc6cf648f7bd4
gustavobraga1001 <100322388+gustavobraga1001@users.noreply.github.com>
2023-06-29 15:42:39 -0300

    second commit

---
+++ src/components/Banner/index.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+function Banner() {
+    return ( 
+        <header className='banner'>
+            <img src = "/images/banner.png"
+            alt = "Banner principal da pagina do organo" />
+        </header>
+        
+    )
+}
+
+export default Banner;
